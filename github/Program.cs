ba4df7d4f4eca8e33e30e3158492e36b2ccfd195
feched <vai-wked@mail.ru>
2024-02-26 21:06:46 +0300

    Добавьте файлы проекта.

---
+++ github/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int x;
+Console.WriteLine("Введите число х");
+x = Convert.ToInt32(Console.ReadLine());
+// решение задачи
+Console.WriteLine($"y={(5 * x) + 20}");
+
+//возведение в степень
+Console.WriteLine(Math.Pow(x, 2));
+
+//возведение в корень
+Console.WriteLine(Math.Sqrt(x));
+
+// логарифм
+Console.WriteLine(Math.Log10(x));
